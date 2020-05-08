7918cab4f8e99d0e98047a39c9e7241cb66faa9c
unknown <darques@cifpfbmoll.eu>
2020-05-08 20:07:58 +0200

    feat: Afegir endpoint nombre contactes

--- src/main/java/org/formacio/mvc/AgendaController.java
+++ src/main/java/org/formacio/mvc/AgendaController.java
@@ -3,11 +3,19 @@ package org.formacio.mvc;
 import org.formacio.repositori.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AgendaController {
 
 	@Autowired
 	public AgendaService agenda;
+	
+	@RequestMapping("/nombre")
+	@ResponseBody
+	public int nombre() {
+		return agenda.nombreContactes();
+	}
 	
 }
