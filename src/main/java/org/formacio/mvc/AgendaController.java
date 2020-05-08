5a684aade7f2700fa893073dc68f0e2a74ad324c
unknown <darques@cifpfbmoll.eu>
2020-05-08 20:20:55 +0200

    feat: Retornar json al client al accedir al endpoint contacte/id

--- src/main/java/org/formacio/mvc/AgendaController.java
+++ src/main/java/org/formacio/mvc/AgendaController.java
@@ -1,8 +1,10 @@
 package org.formacio.mvc;
 
 import org.formacio.repositori.AgendaService;
+import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,4 +25,11 @@ public class AgendaController {
 	public String telefon(String id) {
 		return agenda.recupera(id).getTelefon();
 	}
+	
+	@RequestMapping("/contacte/{clau}")
+	@ResponseBody
+	public Persona contacte(@PathVariable String clau) {
+		return agenda.recupera(clau);
+	}
+	
 }
